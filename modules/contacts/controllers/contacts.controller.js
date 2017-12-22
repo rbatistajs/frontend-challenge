e492feb756008c52e2179ebe43c7739af40bbdbe
Yuri Reis <yuri.reis@msn.com>
2017-12-21 23:44:25 -0200

    Frontend Challenge

---
+++ modules/contacts/controllers/contacts.controller.js
@@ -0,0 +1,46 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('app.contacts')
+    .controller('contactsController', contactsController);
+
+  function contactsController (
+    $rootScope,
+    $scope,
+    $mdBottomSheet,
+    $mdSidenav,
+    $mdDialog,
+    contactsService
+  ) {
+    'ngInject';
+
+    $rootScope.$on('update_list', init);
+    $scope.menu = [{
+      link : '',
+      title: 'Contatos',
+      icon: 'group'
+    }];
+    $scope.message = "Oops! Sem contatos!";
+    $scope.showAdd = showAdd;
+    $scope.init = init;
+
+    init();
+
+    function init() {
+      contactsService.getAllContacts().then(function (response) {
+        $scope.contacts = response.objects;
+      });
+    }
+
+    function showAdd (ev) {
+      $mdDialog.show({
+        templateUrl: 'modules/theme/views/create-contact-dialog.controller.html',
+        targetEvent: ev,
+        controller: 'createContactController'
+      });
+    }
+
+  }
+
+})();
